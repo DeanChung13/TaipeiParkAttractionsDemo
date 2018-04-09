25c9d5a6876eb4136832cd7c4c5bed285825966a
Dean.Chung <deanchung@homemac.com>
2018-04-09 13:52:03 +0800

    Create demo list with RootViewController

--- TaipeiParkDemo/ViewController/RootViewController.h
+++ TaipeiParkDemo/ViewController/RootViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  RootViewController.h
 //  TaipeiParkDemo
 //
 //  Created by DeanChung on 2018/4/9.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface RootViewController : UIViewController
 
 @end
-
